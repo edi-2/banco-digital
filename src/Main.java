bc45e2c14cf17d0b79e1d3e72b51eb5e54745ae8
edi-2 <edilson.mcosta@yahoo.com.br>
2022-09-29 13:17:37 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
 		venilton.setNome("Venilton");
 		
 		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Conta cp = new ContaPoupanca(venilton);
 
 		cc.depositar(100);
-		cc.transferir(100, poupanca);
+		cc.transferir(100, cp);
 		
 		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
+		cp.imprimirExtrato();
 	}
 
 }
